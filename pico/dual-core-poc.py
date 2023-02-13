a14e40109d0bbbfdf51b8d5598748897b4d0e1db
adam4521 <akjaworski@gmail.com>
2023-02-13 11:41:02 +0000

    fixes and annotations

--- pico/dual-core-poc.py
+++ pico/dual-core-poc.py
@@ -8,12 +8,9 @@ import gc
 # Using this article for hints
 # https://bytesnbits.co.uk/multi-thread-coding-on-the-raspberry-pi-pico-in-micropython/
 
-BUFFER_SIZE = 32   # samples
+BUFFER_SIZE = 64   # number of samples, actual buffer bytes is this *8 bytes per sample
 
 
-
-# Buffer memory visible to both threads
-# single contiguous mutable bytearray for previous samples
 # These variables are in global namespace and are available to both CPU cores
 led = machine.Pin(25, machine.Pin.OUT)
 ring_buffer = bytearray(BUFFER_SIZE * 8)
@@ -23,21 +20,22 @@ running = True         # flag variable to indicate if the program has terminated
 sample_value = 0       # dummy data for storing into the buffer
 
 
-# This function runs periodically in Core 0
+# This function runs in Core 0
 def generate_data():
     global ring_buffer, in_ptr, print_signal, sample_value
-    ring_buffer[in_ptr:in_ptr+1] = sample_value.to_bytes(2, 'big')   
-    ring_buffer[in_ptr+1:in_ptr+2] = sample_value.to_bytes(2, 'big')   
-    ring_buffer[in_ptr+2:in_ptr+3] = sample_value.to_bytes(2, 'big')   
-    ring_buffer[in_ptr+3:in_ptr+4] = sample_value.to_bytes(2, 'big')   
-    in_ptr = (in_ptr+1) % (BUFFER_SIZE*8)
-    sample_value = (sample_value+1) % 65536
-    # Indicate to the other thread when it's time to print
-    if in_ptr == BUFFER_SIZE*4:
-        print_signal = 'a'
-    elif in_ptr == 0:
-        print_signal = 'b'
-
+    while running == True:
+        ring_buffer[in_ptr:in_ptr+2] = sample_value.to_bytes(2, 'big')   
+        ring_buffer[in_ptr+2:in_ptr+4] = sample_value.to_bytes(2, 'big')   
+        ring_buffer[in_ptr+4:in_ptr+6] = sample_value.to_bytes(2, 'big')   
+        ring_buffer[in_ptr+6:in_ptr+8] = sample_value.to_bytes(2, 'big')   
+        in_ptr = (in_ptr+8) % (BUFFER_SIZE*8)
+        sample_value = (sample_value+1) % 65536
+        # Indicate to the other thread when it's time to print
+        if in_ptr == BUFFER_SIZE*4:
+            print_signal = 'a'
+        elif in_ptr == 0:
+            print_signal = 'b'
+        #time.sleep(0.005)
 
 
 # This function runs in Core 1
@@ -60,10 +58,9 @@ def print_data():
 
 
 def main():
-    _thread.start_new_thread(print_data, ())
-    while 1:
-        generate_data()
-        time.sleep(0.001)
+    _thread.start_new_thread(print_data, ())  # Start Core 1 (printing)
+    generate_data()                           # Start Core 0 (generating)
+
 
 
 # run from here
@@ -74,7 +71,7 @@ if __name__ == '__main__':
         print('Proceeding...')
         main()
     except KeyboardInterrupt:
-        # Flag to Core 1 that it's time to stop!
+        # Flag to both loops that it's time to stop!
         running = False
         print('Interrupted')
         

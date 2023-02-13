1b3ea70d9bfc406e938f3c03382758370b12f56e
adam4521 <akjaworski@gmail.com>
2023-02-13 09:48:23 +0000

    Updated with led and more comments

--- pico/dual-core-poc.py
+++ pico/dual-core-poc.py
@@ -3,31 +3,35 @@ import machine
 import binascii
 import _thread
 import sys
-import random
 import gc
 
 # Using this article for hints
 # https://bytesnbits.co.uk/multi-thread-coding-on-the-raspberry-pi-pico-in-micropython/
 
-BUFFER_SIZE = 16   # samples
+BUFFER_SIZE = 32   # samples
 
-# pin and SPI setup
-led = machine.Pin(25, machine.Pin.OUT)
+
 
 # Buffer memory visible to both threads
 # single contiguous mutable bytearray for previous samples
+# These variables are in global namespace and are available to both CPU cores
+led = machine.Pin(25, machine.Pin.OUT)
 ring_buffer = bytearray(BUFFER_SIZE * 8)
 in_ptr = 0             # buffer pointer used for charging the buffer
 print_signal = ''      # flag variable to indicate which portion of the buffer to print
 running = True         # flag variable to indicate if the program has terminated
+sample_value = 0       # dummy data for storing into the buffer
 
+
+# This function runs periodically in Core 0
 def generate_data():
-    global ring_buffer, in_ptr, print_signal
-    ring_buffer[in_ptr:in_ptr+1] = random.randrange(65535).to_bytes(2, 'big')   
-    ring_buffer[in_ptr+1:in_ptr+2] = random.randrange(65535).to_bytes(2, 'big')   
-    ring_buffer[in_ptr+2:in_ptr+3] = random.randrange(65535).to_bytes(2, 'big')   
-    ring_buffer[in_ptr+3:in_ptr+4] = random.randrange(65535).to_bytes(2, 'big')   
+    global ring_buffer, in_ptr, print_signal, sample_value
+    ring_buffer[in_ptr:in_ptr+1] = sample_value.to_bytes(2, 'big')   
+    ring_buffer[in_ptr+1:in_ptr+2] = sample_value.to_bytes(2, 'big')   
+    ring_buffer[in_ptr+2:in_ptr+3] = sample_value.to_bytes(2, 'big')   
+    ring_buffer[in_ptr+3:in_ptr+4] = sample_value.to_bytes(2, 'big')   
     in_ptr = (in_ptr+1) % (BUFFER_SIZE*8)
+    sample_value = (sample_value+1) % 65536
     # Indicate to the other thread when it's time to print
     if in_ptr == BUFFER_SIZE*4:
         print_signal = 'a'
@@ -35,16 +39,22 @@ def generate_data():
         print_signal = 'b'
 
 
+
+# This function runs in Core 1
 def print_data():
-    global ring_buffer, print_signal, running
+    global ring_buffer, print_signal, running, led
     while running == True:
-        # Select the correct portion of buffer
+        # Print the correct portion of buffer
         if print_signal == 'a':
             print_signal = ''
+            led.on()
             print(binascii.hexlify(ring_buffer[0:BUFFER_SIZE*4]))
+            led.off()
         elif print_signal == 'b':
             print_signal = ''
+            led.on()
             print(binascii.hexlify(ring_buffer[BUFFER_SIZE*4:BUFFER_SIZE*8]))
+            led.off()
         # There may be issues with automatic GC on Core 1
         gc.collect()
 
@@ -64,6 +74,7 @@ if __name__ == '__main__':
         print('Proceeding...')
         main()
     except KeyboardInterrupt:
+        # Flag to Core 1 that it's time to stop!
         running = False
         print('Interrupted')
         

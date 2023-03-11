89732f7f0af832782852184d1328ad4bd9965b3d
Adam Jaworski <akjaworski@gmail.com>
2023-03-11 11:56:33 +0000

    Continuing bug fixes.

--- pico/spi-8.py
+++ pico/spi-8.py
@@ -6,10 +6,13 @@ from machine import Pin
 import _thread
 import sys
 
+DEBUG_MODE = True
 
 # Buffer memory visible to both threads
 BUFFER_SIZE = 256          # number of samples in a buffer
 BUFFER_SIZE_BIT_MASK = BUFFER_SIZE * 8 - 1    # bit mask to circulate the buffer pointer
+B1 = 0                     # beginning of 1st half of ring buffer
+B2 = BUFFER_SIZE // 2      # beginning of 2nd half
 
 
 # pin and SPI setup
@@ -106,13 +109,25 @@ def setup_adc(spi, cs, reset):
     
     
 # interrupt handler for data ready pin
+# this function runs in Core 1
 def adc_read_handler(dr_adc):
-    global spi_buffer, active_buffer, in_ptr
+    global spi_buffer, ring_buffer, in_ptr
     spi_adc.readinto(spi_buffer)
-    active_buffer[in_ptr : in_ptr+8] = spi_buffer
-    in_ptr = (in_ptr + 8) & BUFFER_SIZE_BIT_MASK    # % BUFFER_SIZE * 8
+    ring_buffer[in_ptr : in_ptr+8] = spi_buffer
+    # 'anding' the pointer with a bit mask makes the buffer pointer
+    # return to zero without needing an 'if' conditional
+    in_ptr = (in_ptr + 8) & BUFFER_SIZE_BIT_MASK    # BUFFER_SIZE * 8 - 1
 
 
+# This function and loop runs on Core 1
+def sample_data():
+    # bind the sampler handler to a falling edge transition on the DR pin
+    dr_adc.irq(trigger = Pin.IRQ_FALLING, handler = adc_read_handler, hard=False)
+    # a do nothing loop now runs in idle time until the program is stopped
+    while running == True:
+        1
+        
+        
 # interrupt handler for reset pin (commanded from Pi)    
 def pico_restart(reset_me):
     machine.reset()
@@ -123,38 +138,26 @@ def initialise():
     setup_adc(spi_adc, cs_adc, reset_adc)
 
 
-# This function runs in Core 1
-def print_data():
-    global buffer1, buffer2, print_signal, running, for_real
-    last_print_signal = 0
-    while running == True:
-        if print_signal == last_print_signal:
-            continue
-        # print signal has changed
-        last_print_signal = print_signal
-        if print_signal == 1:
-            boardled.on()
-            if for_real == True:
-                sys.stdout.buffer.write(buffer1)
-            else:
-                sys.stdout.write('1')
-            time.sleep(0.3)
-            boardled.off()
-        elif print_signal == 2:
-            boardled.on()
-            if for_real == True:
-                sys.stdout.buffer.write(buffer2)
-            else:
-                sys.stdout.write('2')
-            time.sleep(0.3)
-            boardled.off()
-        # There may be issues with automatic GC on Core 1
-        # let's not allocate memory here
-        # gc.collect()
+def print_buffer(buffer_section):
+    boardled.on()
+    if buffer_section == 1:
+        if DEBUG_MODE == True:
+            sys.stdout.write('1')
+        else:
+            # write out the first half of the buffer
+            sys.stdout.buffer.write(ring_buffer[:B2])
+    elif buffer_section == 2:
+        if DEBUG_MODE == True:
+            sys.stdout.write('2')
+        else:
+            # write out the second half of the buffer
+            sys.stdout.buffer.write(ring_buffer[B2:])
+    time.sleep(1)
+    boardled.off()
 
 
 def main():
-    global buffer1, buffer2, print_signal, active_buffer, spi_buffer, in_ptr, running, for_real
+    global ring_buffer, spi_buffer, in_ptr, running
     # waiting in case of lock up, opportunity to cancel
     print('PICO starting up.')
     time.sleep(1)
@@ -165,53 +168,46 @@ def main():
     # configure the ADC
     print('Initialising ADC...')
     initialise()
+    time.sleep(1)
 
-    time.sleep(5)
     print('Allocating bytearray buffers and setting flags...')    
     # two contiguous mutable bytearrays for samples
-    buffer1 = bytearray(BUFFER_SIZE * 8)  # 2 bytes per channel, 4 channels
-    buffer2 = bytearray(BUFFER_SIZE * 8)
-    active_buffer = buffer1     # pointer to the currently active buffer
-
+    ring_buffer = bytearray(BUFFER_SIZE * 8)  # 2 bytes per channel, 4 channels
     spi_buffer = bytearray(8)   # temporary buffer for reading one sample via SPI
     in_ptr = 0                  # buffer cell pointer
     running = True              # flag for stopping the program in both cores
-    print_signal = 0            # flag for core 0 to tell core 1 that it's time to print a buffer
-    for_real = False            # flag to optionally run the program without trashing the terminal with binary data
+    time.sleep(1)
 
-    # the printing runs in parallel with the acquisition
-    _thread.start_new_thread(print_data, ())  # Start Core 1 (printing)
-   
-    print('Setting up hardware interrupts...')
+    print('Setting up RESET interrupt...')
     # configure the interrupt handlers
-    # bind the sampler handler to a falling edge transition on the DR pin
-    dr_adc.irq(trigger = Pin.IRQ_FALLING, handler = adc_read_handler, hard=False)
     # bind the reset handler to a falling edge transition on the RESET ME pin
     reset_me.irq(trigger = Pin.IRQ_FALLING, handler = pico_restart, hard=True)
-    time.sleep(5)
+    time.sleep(1)
 
-    # command ADC to repeatedly refresh ADC registers, by holding CS* low
+    # the sampling process runs on Core 1
+    print('Starting the sampling process on Core 1...')
+    _thread.start_new_thread(sample_data, ())  # Start Core 1 (sampling)
+    time.sleep(1)
+    
+    # now command ADC to repeatedly refresh ADC registers, by holding CS* low
     # pico will successively read these registers
     # each time the DR* pin is activated
+    print("Commanding ADC to start sampling...")
     cs_adc.value(0)
     spi_adc.write(bytes([0b01000001]))
-
+    time.sleep(1)
+    
     print('Entering main loop...')
     while running == True:
 
-        if in_ptr == 0:
-            # time to flip the buffers and print
-            if active_buffer == buffer1:
-                active_buffer = buffer2
-                print_signal = 1
-            else:
-                active_buffer = buffer1
-                print_signal = 2
-
-            # now wait until in_ptr has advanced
-            while in_ptr == 0:
+        if in_ptr == B1:
+            print_buffer(2)
+            while in_ptr == B1:
                 1
-
+        elif in_ptr == B2:
+            print_buffer(1)
+            while in_ptr == B2:
+                1
  
 
 # run from here

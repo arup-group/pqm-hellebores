0f9fe72cd5def78359051d3778235f6dd7aef555
Adam Jaworski <akjaworski@gmail.com>
2023-03-16 18:28:00 +0000

    minor tweaks for tests

--- pi/scaler.py
+++ pi/scaler.py
@@ -4,23 +4,23 @@
 
 import sys
 
-SF1 = 0.01
-SF2 = 0.03
+SF0 = 0.01
+SF1 = 0.03
+SF2 = 0.05
 SF3 = 0.05
-SF4 = 0.06
-INTERVAL = 1000.0/15625   # milliseconds
+INTERVAL = 1000.0/7812.5   # milliseconds
 
 
 def from_twos_complement(v):
     return -(v & 0x8000) | (v & 0x7fff)
 
 
-def scale(sample, i, interval, sf1, sf2, sf3, sf4):
+def scale(sample, i, interval, sf0, sf1, sf2, sf3):
     return (interval*i,\
-             from_twos_complement(sample[1])*sf1,\
-             from_twos_complement(sample[2])*sf2,\
-             from_twos_complement(sample[3])*sf3,\
-             from_twos_complement(sample[4])*sf4)
+             from_twos_complement(sample[1])*sf0,\
+             from_twos_complement(sample[2])*sf1,\
+             from_twos_complement(sample[3])*sf2,\
+             from_twos_complement(sample[4])*sf3)
 
 
 def main():
@@ -29,7 +29,7 @@ def main():
         line = line.rstrip()
         try:
             sample = scale([int(w.strip(), base=16) for w in line.split()],\
-                              i, INTERVAL, SF1, SF2, SF3, SF4)
+                              i, INTERVAL, SF0, SF1, SF2, SF3)
         except ValueError:
             print('scaler.py, main(): Failed to read "' + line + '".', file=sys.stderr)
         print('{:12.4f} {:10.3f} {:10.3f} {:10.3f} {:10.3f}'.format(*sample))

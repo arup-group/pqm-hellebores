115ac5ad47c8a7a8ca43afcb94726096c83065c8
adam4521 <akjaworski@gmail.com>
2023-04-21 16:49:57 +0100

    fixed bugs in trigger.py

--- pi/trigger.py
+++ pi/trigger.py
@@ -12,7 +12,7 @@ INPUT_BUFFER_SIZE = 65535                   # size of circular sample buffer
 def get_settings():
     global sample_rate, time_axis_per_division, pre_trigger_time, post_trigger_time, trigger_direction,\
                trigger_channel, trigger_threshold, interval, post_trigger_samples, pre_trigger_samples, \
-               holdoff_threshold
+               frame_samples, holdoff_samples
     try:
         f = open("settings.json", "r")
         js = json.loads(f.read())
@@ -20,17 +20,11 @@ def get_settings():
         sample_rate             = js['sample_rate']
         time_axis_per_division  = js['time_axis_per_division']
         pre_trigger_time        = js['time_axis_pre_trigger_divisions'] * time_axis_per_division 
-        post_trigger_time       = (js['time_axis_divisions'] - js['time_axis_pre_trigger_divisions']) * js['time_axis_per_division']
+        post_trigger_time       = js['time_axis_divisions'] * time_axis_per_division - pre_trigger_time
         trigger_direction       = js['trigger_direction']
         trigger_channel         = js['trigger_channel']
         trigger_threshold       = js['trigger_threshold']
         interval                = 1000.0 / js['sample_rate']
-        post_trigger_samples    = int(post_trigger_time / interval)
-        pre_trigger_samples     = int(pre_trigger_time / interval)
-        # we set a hold-off threshold (minimum number of samples to next trigger) to be slightly less
-        # than one full screenful of data
-        holdoff_threshold       = 0.9 * (pre_trigger_samples + post_trigger_samples)
-
     except:
         print("trigger.py, get_settings(): couldn't read settings.json, using defaults.", file=sys.stderr)
         sample_rate             = 7812.5
@@ -41,9 +35,15 @@ def get_settings():
         trigger_channel         = 3
         trigger_threshold       = 0.0
         interval                = 1000.0 / 7812.5
-        post_trigger_samples    = int(post_trigger_time / interval)
-        pre_trigger_samples     = int(pre_trigger_time / interval)
-        holdoff_threshold       = 0.9 * (pre_trigger_samples + post_trigger_samples)
+
+    post_trigger_samples    = int(post_trigger_time / interval)
+    pre_trigger_samples     = int(pre_trigger_time / interval)
+    frame_samples           = pre_trigger_samples + post_trigger_samples
+    # we set a hold-off threshold (minimum number of samples to next trigger) to be slightly less
+    # (2ms) than one full screenful of data
+    holdoff_samples         = frame_samples - int(0.002 * sample_rate)
+    sys.stderr.write(f'pre_trig {pre_trigger_samples}, post_trig {post_trigger_samples}, frame_samples {frame_samples}')
+    
 
  
 def settings_handler(signum, frame):
@@ -60,20 +60,20 @@ def next_index(index):
 
 # three samples, channel selector and threshold/direction criteria
 # returns true/false 
-def trigger_detect(buf, ii, ch, threshold, direction):
+def trigger_detect(buf, i, ch, threshold, direction):
     trigger = False
     interpolation_fraction = 0.0
-    ci = ch + 1  # channel index is channel number plus 1 (time is at index 0)
+    ci = ch + 1  # channel index is channel number plus 1 (because time data is at index 0)
     if direction == 'rising':
-        trigger = (buf[ii][ci] > threshold) and (buf[prev_index(ii)][ci] < threshold) and \
-                      (buf[prev_index(prev_index(ii))][ci] < threshold)
+        trigger = (buf[i][ci] > threshold) and (buf[prev_index(i)][ci] < threshold) and \
+                      (buf[prev_index(prev_index(i))][ci] < threshold)
     elif direction == 'falling':
-        trigger = (buf[ii][ci] < threshold) and (buf[prev_index(ii)][ci] > threshold) and \
-                      (buf[prev_index(prev_index(ii))][ci] > threshold)
+        trigger = (buf[i][ci] < threshold) and (buf[prev_index(i)][ci] > threshold) and \
+                      (buf[prev_index(prev_index(i))][ci] > threshold)
     else:
         print('trigger_detect(): selected direction option not implemented', file=sys.stderr)
     if trigger == True:
-        interpolation_fraction = buf[ii][ci] / (buf[ii][ci] - buf[prev_index(ii)][ci])
+        interpolation_fraction = buf[i][ci] / (buf[i][ci] - buf[prev_index(i)][ci])
     return trigger, interpolation_fraction
 
 
@@ -95,7 +95,7 @@ def clear_buffer():
 def main():
     global sample_rate, time_axis_per_division, pre_trigger_time, post_trigger_time, trigger_direction,\
                trigger_channel, trigger_threshold, interval, post_trigger_samples, pre_trigger_samples, buf, \
-               holdoff_threshold
+               frame_samples, holdoff_samples
     get_settings()
     # we make a buffer to temporarily hold a history of samples -- this allows us to output
     # waveform samples 'before the trigger'
@@ -108,7 +108,9 @@ def main():
     ii = 0    # input index (into buf, filling buffer) 
     oi = 0    # output index (out of buf, draining buffer)
     # output counter, number of samples output in current frame
-    oc = pre_trigger_samples + post_trigger_samples
+    oc = 0
+    # holdoff counter, used to prevent re-triggering for a preset number of samples
+    hc = 0
     # the exact trigger position is normally somewhere between samples
     interpolation_fraction = 0.0
 
@@ -129,33 +131,36 @@ def main():
         # DRAINING BUFFER 
         # if hold off is clear, and we are not currently triggered, check to see if any outstanding 
         # samples qualify for a trigger
-        if (oc >= holdoff_threshold) and not triggered:
-            while oi != ii:
-                triggered, interpolation_fraction = trigger_detect(buf, oi, trigger_channel,\
-                                                        trigger_threshold, trigger_direction) 
-                if triggered:
-                    # we have found a valid trigger
-                    # figure out the 'pre-trigger' index and set the output pointer to that position
-                    # set an output sample counter, and then exit this loop
-                    oi = (oi - pre_trigger_samples) % INPUT_BUFFER_SIZE
-                    oc = 0 
-                    break
+        while not triggered and (hc >= holdoff_samples) and (oi != ii):
+            triggered, interpolation_fraction = trigger_detect(buf, oi, trigger_channel,\
+                                                   trigger_threshold, trigger_direction) 
+            if triggered == True:
+                # we have found a valid trigger
+                # figure out the 'pre-trigger' index and set the output pointer to that position
+                # set an output sample counter, and then exit this loop
+                oi = (oi - pre_trigger_samples) % INPUT_BUFFER_SIZE
+                hc = 0
+                oc = 0
+            else:
                 oi = next_index(oi)  
    
-        # if triggered, print out all buffered/outstanding samples up to the input pointer
-        if triggered:
-            while oi != ii:
-                print('{:10.3f} {:10.3f} {:10.3f} {:10.3f} {:10.3f}'.format(interval *\
-                          (oc - pre_trigger_samples), *interpolate(buf[prev_index(oi)][1:],\
-                          buf[oi][1:], interpolation_fraction)))
+        # if triggered, print out all buffered/outstanding samples up to the current input pointer
+        while triggered and (oi != ii):
+            print('{:10.3f} {:10.3f} {:10.3f} {:10.3f} {:10.3f}'.format(interval *\
+                      (oc - pre_trigger_samples), *interpolate(buf[prev_index(oi)][1:],\
+                      buf[oi][1:], interpolation_fraction)))
+            oc = oc + 1
+            # if we've finished a whole frame of data, clear the trigger and position the output
+            # index counter 2ms behind the current input index
+            if oc >= frame_samples:
+                triggered = False
+                oi = (ii - int(0.002 * sample_rate)) % INPUT_BUFFER_SIZE
+            else:
                 oi = next_index(oi)
-                oc = oc + 1
-
-        # if we've finished a whole frame of data, clear the trigger and back up the output
-        # index counter by one division
-        if oc >= pre_trigger_samples + post_trigger_samples:
-            triggered = False
-            oi = (oi - int(sample_rate * time_axis_per_division/1000)) % INPUT_BUFFER_SIZE
+            
+        # increment the holdoff counter, this has to be done once per input sample/outer loop
+        hc = hc + 1
+ 
 
 if __name__ == '__main__':
     main()

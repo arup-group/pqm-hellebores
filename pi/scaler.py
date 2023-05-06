7b76ef6d72d6443328003cc0f0851df15e01b7d2
adam4521 <akjaworski@gmail.com>
2023-05-06 16:20:09 +0100

    output from scaler.py now volts, amps, watts, leakage

--- pi/scaler.py
+++ pi/scaler.py
@@ -12,14 +12,6 @@ def from_twos_complement(v):
     return -(v & 0x8000) | (v & 0x7fff)
 
 
-def scale(sample, i, interval, sf0, sf1, sf2, sf3):
-    return (interval*i,\
-             from_twos_complement(sample[1])*sf0,\
-             from_twos_complement(sample[2])*sf1,\
-             from_twos_complement(sample[3])*sf2,\
-             from_twos_complement(sample[4])*sf3)
-
-
 def main():
     st = settings.Settings(lambda: None)
 
@@ -27,13 +19,25 @@ def main():
     for line in sys.stdin:
         line = line.rstrip()
         try:
-            sample = scale([int(w.strip(), base=16) for w in line.split()],\
-                              i, st.interval, st.scale_c0, st.scale_c1, st.scale_c2, st.scale_c3)
+            # calculate time axis position
+            t = st.interval*i
+            # get channel values as integer array
+            cs = [int(w.strip(), base=16) for w in line.split()][1:]
+            # calculate floating point values, using appropriate scaling factors
+            voltage = from_twos_complement(cs[3]) * st.scale_c3
+            # if st.current_axis_per_division is less than or equal to 0.1 A/div, we use channel 1 for
+            # current measurements. If it is more than 0.1 A/div, we use channel 2.
+            if st.current_axis_per_division <= 0.1:
+                current = from_twos_complement(cs[1]) * st.scale_c1
+            else:
+                current = from_twos_complement(cs[2]) * st.scale_c2
+            power = voltage * current
+            leakage_current = from_twos_complement(cs[0]) * st.scale_c0 
+
         except ValueError:
             print('scaler.py, main(): Failed to read "' + line + '".', file=sys.stderr)
-        print('{:12.4f} {:10.3f} {:10.3f} {:10.3f} {:10.3f}'.format(*sample))
+        print(f'{t :12.4f} {voltage :10.3f} {current :10.3f} {power :10.3f} {leakage_current :10.3f}')
         i = i + 1
-
 
 
 if __name__ == '__main__':

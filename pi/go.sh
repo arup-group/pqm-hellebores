8eaf4318c90f188b3b1c75aac350d3260de048c3
adam4521 <akjaworski@gmail.com>
2023-06-26 13:51:14 +0100

    auto flip between real and simulate modes

--- pi/go.sh
+++ pi/go.sh
@@ -1,4 +1,10 @@
-./rain.py | ./scaler.py | ./trigger.py | ./mapper.py | ./hellebores.py
+#!/bin/bash
 
-
+if [ -f /dev/ttyACM0 ]; then 
+    echo "Running with data sourced from Pico..."
+    ./reader.py | ./scaler.py | ./trigger.py | ./mapper.py | ./hellebores.py
+else
+    echo "Running using generated data..."
+    ./rain.py | ./scaler.py | ./trigger.py | ./mapper.py | ./hellebores.py
+fi
 

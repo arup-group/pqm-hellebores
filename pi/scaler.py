d1ae6f65fe1710b99122f69b49970d7d820385ae
adam4521 <akjaworski@gmail.com>
2023-07-09 15:07:02 +0100

    added calibrations per device

--- pi/scaler.py
+++ pi/scaler.py
@@ -24,20 +24,23 @@ def main():
             # get channel values as integer array
             cs = [int(w.strip(), base=16) for w in line.split()][1:]
             # calculate floating point values, using appropriate scaling factors
-            voltage = (from_twos_complement(cs[3]) + st.adc_offset_trim_c3) * st.scale_c3
+            voltage = (from_twos_complement(cs[3]) + st.adc_offset_trim_c3) \
+                          * st.scale_c3 * st.adc_gain_trim_c3
             # if st.current_axis_per_division is less than or equal to 0.1 A/div,
             # we use channel 1 for current measurements. If it is more than 0.1 A/div,
             # we use channel 2.
             if st.current_axis_per_division <= 0.1:
-                current = (from_twos_complement(cs[1]) + st.adc_offset_trim_c1) * st.scale_c1
+                current = (from_twos_complement(cs[1]) + st.adc_offset_trim_c1) \
+                          * st.scale_c1 * st.adc_gain_trim_c1
             else:
                 # *2 hack here for prototype only, due to having 0.5 ohm sense resistor
                 # instead of 1 ohm per the design.
-                #current = (from_twos_complement(cs[2]) + st.adc_offset_trim_c2) * st.scale_c2
-                current = (from_twos_complement(cs[2]) + st.adc_offset_trim_c2) * st.scale_c2 / 2.0
+                #current = (from_twos_complement(cs[2]) + st.adc_offset_trim_c2) * st.scale_c2 / 2.0
+                current = (from_twos_complement(cs[2]) + st.adc_offset_trim_c2) \
+                              * st.scale_c2 * st.adc_gain_trim_c2
             power = voltage * current
-            leakage_current = (from_twos_complement(cs[0]) + st.adc_offset_trim_c0) * st.scale_c0 
-
+            leakage_current = (from_twos_complement(cs[0]) + st.adc_offset_trim_c0) \
+                              * st.scale_c0 * st.adc_gain_trim_c0
         except ValueError:
             print('scaler.py, main(): Failed to read "' + line + '".', file=sys.stderr)
         print(f'{t :12.4f} {voltage :10.3f} {current :10.5f} {power :10.3f} {leakage_current :12.7f}')

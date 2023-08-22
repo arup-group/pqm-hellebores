dee56191f7dc0602de0ac64d070e6fa6deefbd44
adam4521 <akjaworski@gmail.com>
2023-08-22 17:30:16 +0100

    updated get/show identity files

--- pi/get_identity.py
+++ pi/get_identity.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 import settings
+import subprocess
 
 s = settings.Settings()
+ip = subprocess.run(['hostname', '-I'], capture_output=True, text=True).stdout.rstrip()
 
-print(f'Power quality monitor identity is: {s.identity}')
+print(f'Device identity: {s.identity}')
 print(f'MAC address: {s.mac}')
-
+print(f'IP address: {ip}')
 
---
+++ pi/show_identity.sh
@@ -0,0 +1,2 @@
+./get_identity.py | xcowsay
+

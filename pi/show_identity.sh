9c3f36b1f863ade284d403f14d6358f410ab0069
adam4521 <akjaworski@gmail.com>
2023-08-22 22:09:56 +0100

    added run path check

--- pi/show_identity.sh
+++ pi/show_identity.sh
@@ -1,2 +1,7 @@
-./get_identity.py | xcowsay
+#!/bin/bash
 
+if [ -e /home/pi/pqm-hellebores/pi ]; then
+    /home/pi/pqm-hellebores/pi/get_identity.py | xcowsay
+else
+    ./get_identity.py | xcowsay
+fi

f3957911c9232e9ff433ac92680e862754016e31
adam4521 <akjaworski@gmail.com>
2023-09-17 14:08:23 +0100

    added serial flush

--- pi/go.sh
+++ pi/go.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-# Run the program
+# Figure out if we are running on real hardware or not
 if [ -e /dev/ttyACM0 ]; then 
+    have_pico=1
+fi
+
+# Run the program
+if $have_pico; then
     echo "Running with data sourced from Pico..."
     cd /home/pi/pqm-hellebores/pi
     ./reader.py | ./scaler.py | ./trigger.py | ./mapper.py | ./hellebores.py
@@ -14,16 +19,24 @@ fi
 # Now check the exit code in case we need to do anything
 exit_code=$?
 if [ $exit_code -eq 2 ]; then
-    # Reset the Pico and restart the app
+    if $have_pico; then
+    # Reset the Pico
     echo "Resetting Pico..."
     ./pico_reset.py
     sleep 10
+    # Flush serial interface
+    # open the serial port for reading on file descriptor 2
+    # and then empty it
+    2</dev/ttyACM0
+    while read -t 0 -u 2 discard; do echo "Flushing serial port..."; done
+    # Trampoline: reload the current script and run again
     echo "Restarting $0..."
     exec $0
 elif [ $exit_code -eq 3 ]; then
     # Update software and restart the app
     echo "Updating software from repository..."
     git pull
+    # Trampoline: reload the current script and run again
     echo "Restarting $0..."
     exec $0 
 elif [ $exit_code -eq 4 ]; then

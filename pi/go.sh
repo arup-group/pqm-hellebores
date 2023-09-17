f73017f5b788797a9be62cc08c850ef0afc79feb
adam4521 <akjaworski@gmail.com>
2023-09-17 20:11:34 +0100

    debug serial port test

--- pi/go.sh
+++ pi/go.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 # Figure out if we are running on real hardware or not
-if [ -e /dev/ttyACM0 ]; then 
+if [[ -e /dev/ttyACM0 ]]; then 
     have_pico=1
+else
+    have_pico=0
 fi
 
 # Run the program
-if $have_pico; then
+if [[ $have_pico -eq 1 ]]; then
     echo "Running with data sourced from Pico..."
     cd /home/pi/pqm-hellebores/pi
     ./reader.py | ./scaler.py | ./trigger.py | ./mapper.py | ./hellebores.py
@@ -18,8 +20,8 @@ fi
 # The program finished.
 # Now check the exit code in case we need to do anything
 exit_code=$?
-if [ $exit_code -eq 2 ]; then
-    if $have_pico; then
+if [[ $exit_code -eq 2 ]]; then
+    if [[ $have_pico -eq 1 ]]; then
     # Reset the Pico
     echo "Resetting Pico..."
     ./pico_reset.py
@@ -32,18 +34,18 @@ if [ $exit_code -eq 2 ]; then
     # Trampoline: reload the current script and run again
     echo "Restarting $0..."
     exec $0
-elif [ $exit_code -eq 3 ]; then
+elif [[ $exit_code -eq 3 ]]; then
     # Update software and restart the app
     echo "Updating software from repository..."
     git pull
     # Trampoline: reload the current script and run again
     echo "Restarting $0..."
     exec $0 
-elif [ $exit_code -eq 4 ]; then
+elif [[ $exit_code -eq 4 ]]; then
     # Shut down system
     echo "Shutting down system..."
     exec sudo shutdown -h now
-elif [ $exit_code -ne 0 ]; then
+elif [[ $exit_code -ne 0 ]]; then
     # We have no idea what happened
     echo "The program quit with an unexpected exit code $exit_code. Not good."
 fi

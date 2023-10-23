8890378b7160ed8bc4e0297e08ea9fb265a134d8
adam4521 <akjaworski@gmail.com>
2023-10-23 07:00:32 +0100

    split multimeter and constants from hellebores

---
+++ pi/hellebores_constants.py
@@ -0,0 +1,55 @@
+      
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARK_GREY = (30, 30, 30)
+GREY = (75, 75, 75)
+LIGHT_GREY = (100, 100, 100)
+VERY_LIGHT_GREY = (150, 150, 150)
+PI_SCREEN_SIZE = (800,480)
+SCOPE_BOX_SIZE = (700,480)
+CONTROLS_BOX_SIZE = (100,480)        # main buttons and status texts
+CONTROLS_BOX_POSITION = (800,0)      # top right corner
+SETTINGS_BOX_SIZE = (500,400)        # 'dialog' boxes
+SETTINGS_BOX_POSITION = (690,100)    # top right corner
+BUTTON_SIZE = (90,50) 
+BUTTON_WIDE_SIZE = (180,50) 
+TEXT_SIZE = (90,16)
+TEXT_WIDE_SIZE = (120,16)
+FONT = 'dejavusansmono'
+FONT_SIZE = 14
+SAMPLE_BUFFER_SIZE = 100 
+
+# Default pygame font: freesansbold
+# Ubuntu monospaced fonts:
+# dejavusansmono
+# ubuntumono
+# bitstreamverasansmono
+# nimbusmonops
+# notosansmono
+# notomono
+ 
+# button enumerations
+B_RUNSTOP     = 0
+B_MODE        = 1
+B_HORIZONTAL  = 2
+B_VERTICAL    = 3
+B_TRIGGER     = 4
+B_OPTIONS     = 5
+
+
+# text message cell enumerations
+T_RUNSTOP     = 0
+T_WFS         = 1
+T_TIMEDIV     = 2
+T_VOLTSDIV    = 3
+T_AMPSDIV     = 4
+T_WATTSDIV    = 5
+T_LEAKDIV     = 6
+
+

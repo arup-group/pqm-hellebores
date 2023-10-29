ec7f013b89bfcf881376ae8a69e0e019cfae6f77
adam4521 <akjaworski@gmail.com>
2023-10-29 16:58:07 +0000

    reduce data rate on placeholder calculate.py

--- pi/calculate.py
+++ pi/calculate.py
@@ -10,10 +10,13 @@ import settings
 def main():
     st = settings.Settings()
 
+    i = 0
     for line in sys.stdin:
         line = line.rstrip()
-        print(line)
-
+        # print only one line in 7812 (1 per second, approx)
+        if i == 0:
+            print(line)
+        i = (i + 1) % 7812
     print('Finished calculations.')
 
 if __name__ == '__main__':

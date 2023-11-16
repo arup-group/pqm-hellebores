b9385d3234636018491813f08333b090ee4a22ce
adam4521 <akjaworski@gmail.com>
2023-11-16 18:35:57 +0000

    Improved error detection

--- pi/go.sh
+++ pi/go.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-# Figure out if we are running on real hardware or not
-if [[ -e /dev/ttyACM0 ]]; then
-    have_pico=true
-else
-    have_pico=false
-fi
-
 # Change working directory to the directory of this script
-DIRNAME=$(realpath $(dirname $0))
-cd $DIRNAME
+PROGRAM_DIR=$(realpath $(dirname $0))
+cd $PROGRAM_DIR
 
 # Generate MD5 checksum of the program files and store it in environment variable
 # The environment variables are accessible from within the application
@@ -18,18 +11,28 @@ export MD5SUM=$(cat go.sh rain_bucket.py reader.py scaler.py trigger.py mapper.p
                 hellebores.py hellebores_constants.py hellebores_waveform.py \
                 hellebores_multimeter.py ../pico/spi-dual-core.py | md5sum | cut -d ' ' -f 1)
 export VERSION=$(cat ../VERSION)
+
 # TEMP_DIR: settings.json, error.log and named pipes will be stored here
-export TEMP_DIR="/run/shm/pqm-hellebores"
-[[ -e $TEMP_DIR ]] || mkdir $TEMP_DIR
+# /run/shm is preferred, because it is mounted as RAM disk
+for TD in "/run/shm/pqm-hellebores" "/tmp/pqm-hellebores"; do
+    [[ -e $TD ]] || mkdir $TD
+    if [[ $? -eq 0 ]]; then
+        export TEMP_DIR=$TD
+        break
+    fi
+done
+# if both attempts failed, quit with an error
+if [[ -z $TEMP_DIR ]]; then
+    echo "Check filesystem or write permissions, quitting."
+    exit 1
+fi
 
-echo Running in $DIRNAME
-echo Version $VERSION
-echo MD5 checksum $MD5SUM
-
+# set up the working files
 WAVEFORM_PIPE=$TEMP_DIR/waveform_pipe
 ANALYSIS_PIPE=$TEMP_DIR/analysis_pipe
 MEASUREMENT_LOG_FILE=$TEMP_DIR/pqm.$$.csv
 ERROR_LOG_FILE=$TEMP_DIR/error.log
+
 
 # If they don't already exist, create named pipes (fifos) to receive data from
 # the waveform and calculation processes
@@ -42,23 +45,30 @@ for PIPE_FILE in $WAVEFORM_PIPE $ANALYSIS_PIPE; do
     fi
 done
 
+# Figure out if we are running on real hardware or not
+if [[ -e /dev/ttyACM0 ]]; then
+    READER="./reader.py"
+else
+    READER="./rain_bucket.py ../sample_files/simulated.out"
+fi
+
+echo "Working directory    : $PROGRAM_DIR"
+echo "Temporary files      : $TEMP_DIR"
+echo "Version              : $VERSION"
+echo "MD5 checksum         : $MD5SUM"
+echo "Measurement data     : $READER"
+
 # Start the data pipeline, output feeding the two named pipes
 # Clear old log file
-[[ -e $ERROR_LOG_FILE ]] || rm $ERROR_LOG_FILE
+[[ -e $ERROR_LOG_FILE ]] && rm $ERROR_LOG_FILE
 # Duplicate stderr file descriptor 2 to 4
 # then redirect 2 to file
 exec 4>&2 2>$ERROR_LOG_FILE
 
-if $have_pico; then
-    echo "Running with data sourced from Pico..."
-    READER="./reader.py"
-else
-    echo "Running using previously captured data..."
-    READER="./rain_bucket.py ../sample_files/simulated.out"
-fi
-
 # Run the capture and analysis, feeding two pipe files,
 # passing both pipes as parameters to the GUI
+echo "Starting processing..."
+
 $READER \
 | ./scaler.py \
 | tee >(./analyser.py | tee $MEASUREMENT_LOG_FILE > $ANALYSIS_PIPE) \
@@ -68,6 +78,7 @@ $READER \
 
 # The program finished. NB Programs in the data pipeline all
 # terminate when the pipeline is broken
+echo "Finished processing."
 
 # Capture the exit code from hellebores.py
 # We'll check it's status shortly

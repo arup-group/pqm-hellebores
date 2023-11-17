71ef855918bb94b66536734e6098119a235b78bd
adam4521 <akjaworski@gmail.com>
2023-11-17 20:39:18 +0000

    made self-contained with lxterminal

--- pi/pi-setup/access_me.sh
+++ pi/pi-setup/access_me.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 sleep 10
-ssid=$(iwgetid)
+
 ip_address=$(hostname -I | cut -d ' ' -f 1)
+user=$(whoami)
+this_script=$(realpath $0)
 
-echo My wifi network details:
-echo $ssid
-echo 
-echo My IP address:
-echo $ip_address
-echo
-echo Connect to me remotely using:
-echo ssh $(whoami)@$ip_address
-echo
-echo Follow the configuration instructions to install software.
-echo When finished, disable this bootstrap script by deleting or renaming it.
-echo The script is located at:
-echo $(realpath $0)
+message="My IP address:
+$ip_address
 
+Connect to me remotely using:
+ssh $user@$ip_address
 
+Follow the configuration instructions to install software.
+Disable this bootstrap script by deleting or renaming it.
+The script is located at:
+$this_script
+"
+
+# display the output in a terminal window and wait indefinitely
+lxterminal -t "REMOTE ACCESS FOR SETUP" -e "echo \"$message\"; tail -f /dev/null"
 

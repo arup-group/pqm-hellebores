4ae6914ee61714e024b3305872b9edc43091e0ea
adam4521 <akjaworski@gmail.com>
2023-11-25 19:59:48 +0000

    interpolated script location

--- pi/pi-setup/autorun.sh
+++ pi/pi-setup/autorun.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Create a script and store it in the variable $commands
-# don't expand any variables until the time comes to run it.
-read -r -d '' commands << '_EOF_' 
+# Create a script and store it
+# In this portion, don't expand any variables until
+# the time comes to run it.
+read -r -d '' commands1 << '_EOF_' 
 echo "Waiting 10 seconds to allow network to connect..."
 sleep 10
 clear
 
 ip_address=$(hostname -I | cut -d ' ' -f 1)
 user=$(whoami)
-this_script=$(realpath $0)
 
 echo "My IP address:
 $ip_address
@@ -20,12 +20,18 @@ ssh $user@$ip_address
 Follow the configuration instructions to install software.
 Disable this autorun script by deleting or renaming it.
 This script is located at:
-$this_script
 "
+_EOF_
+
+# Here, we interpolate the location of the script
+read -r -d '' commands2 << _EOF_
+$(realpath $0)
 
 # wait indefinitely
 tail -f /dev/null
 _EOF_
+
+commands="$commands1$commands2"
 
 # Now run the commands in a new terminal window
 if [[ -e /usr/bin/lxterminal ]]; then

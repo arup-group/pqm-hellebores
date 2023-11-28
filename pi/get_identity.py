6b15722a4d4acb02be3f79b7ba2e65d3ce43af57
Adam Jaworski <akjaworski@gmail.com>
2023-11-28 21:55:16 +0000

    removed MAC address retrieve function

--- pi/get_identity.py
+++ pi/get_identity.py
@@ -7,7 +7,6 @@ s = settings.Settings()
 ips = subprocess.run(['hostname', '-I'], capture_output=True, text=True).stdout.rstrip().split()
 
 print(f'Device identity: {s.identity}')
-print(f'MAC address: {s.mac}')
 for ip in ips :
     print(f'IP address: {ip}')
 

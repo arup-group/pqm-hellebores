998b183fa50e3269c90cd2191c821b1fcb569d14
adam4521 <akjaworski@gmail.com>
2023-12-04 06:16:17 +0000

    beginning work on multimeter display

--- pi/hellebores_constants.py
+++ pi/hellebores_constants.py
@@ -1,4 +1,3 @@
-      
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
@@ -17,12 +16,16 @@ CONTROLS_BOX_SIZE = (100,480)        # main buttons and status texts
 CONTROLS_BOX_POSITION = (800,0)      # top right corner
 SETTINGS_BOX_SIZE = (500,400)        # 'dialog' boxes
 SETTINGS_BOX_POSITION = (690,100)    # top right corner
+METER_POSITION = (20,40)
 BUTTON_SIZE = (90,50) 
 BUTTON_WIDE_SIZE = (180,50) 
 TEXT_SIZE = (90,16)
 TEXT_WIDE_SIZE = (120,16)
+TEXT_METER_LABEL_SIZE = (30,64)
+TEXT_METER_SIZE = (170,64)
 FONT = 'font/RobotoMono-Medium.ttf'
 FONT_SIZE = 14
+FONT_METER_SIZE = 42
 SAMPLE_BUFFER_SIZE = 100 
 
 # Default pygame font: freesansbold
--- pi/hellebores_multimeter.py
+++ pi/hellebores_multimeter.py
@@ -20,19 +20,15 @@ class Multimeter:
         self.texts = []
         self.st = st
         self.app_actions = app_actions
-        for s in range(4):
-            t = thorpy.Text('')
-            t.set_size(TEXT_SIZE)
-            if s==0:
-                t.set_font_color(BLACK)
-            else:
-                t.set_font_color(WHITE)
-            self.texts.append(t)
         # create an empty background to draw onto
         self.multimeter_background = pygame.Surface(SCOPE_BOX_SIZE)
         self.multimeter_background.fill(GREY)
         # create the controls
         self.multimeter_controls = self.create_multimeter_controls()
+        # create the readings
+        self.multimeter_readings = self.create_multimeter_readings()
+        # all the elements
+        self.multimeter_elements = [ self.multimeter_controls, self.multimeter_readings ]
 
     def set_text(self, item, value):
         self.texts[item].set_text(value)
@@ -51,6 +47,15 @@ class Multimeter:
 
     def create_multimeter_controls(self):
         """Multimeter controls, on right of screen"""
+        control_texts = []
+        for s in range(4):
+            t = thorpy.Text('')
+            t.set_size(TEXT_SIZE)
+            if s==0:
+                t.set_font_color(BLACK)
+            else:
+                t.set_font_color(WHITE)
+            control_texts.append(t)
         button_setup = [
             ('Run/Stop', self.app_actions.start_stop),
             ('Mode', lambda: self.app_actions.set_updater('mode')), 
@@ -58,9 +63,37 @@ class Multimeter:
             ('Options', lambda: self.app_actions.set_updater('options'))
             ]
         buttons = [ configure_button(BUTTON_SIZE, bt, bf) for bt, bf in button_setup ]
-        multimeter_controls = thorpy.Box([ *self.texts[0:2], *buttons, *self.texts[2:] ])
+        # Now add the multimeter controls
+        multimeter_controls = thorpy.Box([ *control_texts[0:2], *buttons, *control_texts[2:] ])
         multimeter_controls.set_topright(*CONTROLS_BOX_POSITION)
         multimeter_controls.set_bck_color(LIGHT_GREY)
         for e in multimeter_controls.get_all_descendants():
             e.hand_cursor = False    
+        self.texts = control_texts
         return multimeter_controls
+
+    def create_multimeter_readings(self):
+        """Multimeter readings, on main part of screen"""
+        reading_texts = []
+        for s in [ ('230.01 ', TEXT_METER_SIZE, FONT_METER_SIZE, YELLOW),
+                   ('  0.534', TEXT_METER_SIZE, FONT_METER_SIZE, YELLOW),
+                   (' 20.344', TEXT_METER_SIZE, FONT_METER_SIZE, YELLOW),
+                   (' 34.234', TEXT_METER_SIZE, FONT_METER_SIZE, YELLOW),
+                   ('  0.762', TEXT_METER_SIZE, FONT_METER_SIZE, YELLOW),
+                   ('  0.452', TEXT_METER_SIZE, FONT_METER_SIZE, YELLOW),
+                   ('V  ', TEXT_METER_LABEL_SIZE, FONT_SIZE, WHITE),
+                   ('A  ', TEXT_METER_LABEL_SIZE, FONT_SIZE, WHITE),
+                   ('W  ', TEXT_METER_LABEL_SIZE, FONT_SIZE, WHITE),
+                   ('VA ', TEXT_METER_LABEL_SIZE, FONT_SIZE, WHITE),
+                   ('PF ', TEXT_METER_LABEL_SIZE, FONT_SIZE, WHITE),
+                   ('kWh', TEXT_METER_LABEL_SIZE, FONT_SIZE, WHITE) ]:
+            t = thorpy.Text(s[0])
+            t.set_size(s[1])
+            t.set_font_size(s[2])
+            t.set_font_color(s[3])
+            reading_texts.append(t)
+        multimeter_readings = thorpy.Group(reading_texts, mode='grid', gap=0, margins=(0,0), ny=6)
+        multimeter_readings.set_topleft(*METER_POSITION)
+        return multimeter_readings
+
+

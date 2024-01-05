7ae79d85fd7e25517663d09508b2e66a21bec661
adam4521 <akjaworski@gmail.com>
2024-01-05 12:00:02 +0000

    improved pipe abstraction for windows

--- windows/mswin_pipes.py
+++ windows/mswin_pipes.py
@@ -11,68 +11,88 @@ import win32pipe
 import win32file
 import settings
 
-def open_pipe_for_input(pipe_name):
-    pipe = None
-    retries = 6
-    while pipe == None and retries > 0:
-        try:
-            pipe = win32file.CreateFile(
-                pipe_name,
-                win32file.GENERIC_READ | win32file.GENERIC_WRITE,
-                0,
-                None,
-                win32file.OPEN_EXISTING,
-                0,
-                None
-            )
-            res = win32pipe.SetNamedPipeHandleState(pipe, win32pipe.PIPE_READMODE_MESSAGE, None, None)
-        except:
-            print(f"mswin_pipes.open_pipe_for_input(): waiting for pipe {pipe_name} to become available.", file=sys.stderr) 
+class Pipe:
+    def __init__(self, pipe_name, mode):
+        self.pfh = None  # pipe file handle
+        if mode == 'r':
+            self.open_existing_pipe(pipe_name)
+        elif mode == 'w':
+            self.open_new_pipe(pipe_name)
+        else:
+            print(f"Can't open pipe with this mode", file=sys.stderr)
+
+    # these two functions facilitate use of the object with the 'with' syntax
+    # so that we close the pipe properly even if there's an exception
+    def __enter__(self):
+        return self
+    
+    def __exit__(self):
+        self.close()
+
+    def open_existing_pipe(self, pipe_name):
+        pipe = None
+        retries = 6
+        while pipe == None and retries > 0:
+            try:
+                pipe = win32file.CreateFile(
+                    pipe_name,
+                    win32file.GENERIC_READ | win32file.GENERIC_WRITE,
+                    0,
+                    None,
+                    win32file.OPEN_EXISTING,
+                    0,
+                    None
+                )
+                res = win32pipe.SetNamedPipeHandleState(pipe, win32pipe.PIPE_READMODE_MESSAGE, None, None)
+            except:
+                print(f"mswin_pipes.open_existing_pipe(): waiting for pipe {pipe_name} to become available.", file=sys.stderr) 
             time.sleep(1.0)
             retries = retries - 1
-    if pipe == None:
-        print(f"mswin_pipes.open_pipe_for_input(): couldn't open pipe {pipe_name}, quitting.", file=sys.stderr)
-        raise SystemExit
-    return pipe
+        if pipe == None:
+            print(f"mswin_pipes.open_existing_pipe(): couldn't open pipe {pipe_name}, quitting.", file=sys.stderr)
+            raise SystemExit
+        self.pfh = pipe
+        return self.pfh
 
 
-def open_pipe_for_output(pipe_name):
-    try:
-        pipe = win32pipe.CreateNamedPipe(
-            pipe_name,
-            win32pipe.PIPE_ACCESS_DUPLEX,
-            win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
-            1, 65536, 65536,
-            0,
-            None)
-        win32pipe.ConnectNamedPipe(pipe, None)
-    except:
-        print("Couldn't open the requested pipe for output, quitting.", sys.stderr)
-        raise SystemExit
-    return pipe
+    def open_new_pipe(self, pipe_name):
+        pipe = None
+        try:
+            pipe = win32pipe.CreateNamedPipe(
+                pipe_name,
+                win32pipe.PIPE_ACCESS_DUPLEX,
+                win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+                1, 65536, 65536,
+                0,
+                None)
+            win32pipe.ConnectNamedPipe(pipe, None)
+        except:
+            print(f"mswin_pipes.open_new_pipe(): couldn't open pipe {pipe_name}, quitting.", file=sys.stderr)
+            raise SystemExit
+        self.pfh = pipe
+        return self.pfh
 
 
-def read_from_pipe(pipe):
-    _, received_bytes = win32file.ReadFile(pipe, 64*1024)
-    return str(received_bytes, encoding='utf-8')
+    def readline(self):
+        _, received_bytes = win32file.ReadFile(self.pfh, 64*1024)
+        return str(received_bytes, encoding='utf-8')
 
 
-def write_to_pipe(line, pipe):
-    win32file.WriteFile(pipe, str.encode(line))
+    def writeline(self, line):
+        win32file.WriteFile(self.pfh, str.encode(line))
 
 
-def is_data_available(pipe, t):
-    t0 = time.time()
-    while True:
-        _, n, _ = win32pipe.PeekNamedPipe(pipe, 0)
-        if n > 0:
-            return True
-        if time.time() - t0 > t:
-            return False
+    def is_data_available(self, t):
+        t0 = time.time()
+        while True:
+            _, n, _ = win32pipe.PeekNamedPipe(self.pfh, 0)
+            if n > 0:
+                return True
+            if time.time() - t0 > t:
+                return False
 
-
-def close_pipe(pipe):
-    win32file.CloseHandle(pipe)
+    def close(self):
+        win32file.CloseHandle(self.pfh)
 
 
 def main():
@@ -83,36 +103,24 @@ def main():
     try:
         command = sys.argv[1]
         if command == 'read':
-            pipe = open_pipe_for_input(sys.argv[2])
-            while True:
-                sys.stdout.write(read_from_pipe(pipe))
-            close_pipe(pipe)
+            with Pipe(sys.argv[2], 'r') as p:
+                while True:
+                    sys.stdout.write(p.readline())
     
         elif command == 'write':
-            pipe = open_pipe_for_output(sys.argv[2])
-            while True:
-                line = sys.stdin.readline()
-                if line:
-                    write_to_pipe(line, pipe)
-                else:
-                    break
-            close_pipe(pipe)
-
+            with Pipe(sys.argv[2], 'w') as p:
+                for line in sys.stdin:
+                    p.writeline(line)
+                    
         elif command == 'tee':
-            pipe1 = open_pipe_for_output(sys.argv[2])
-            pipe2 = open_pipe_for_output(sys.argv[3])
-            while True:
-                line = sys.stdin.readline()
-                if line:
-                    write_to_pipe(line, pipe1)
-                    write_to_pipe(line, pipe2)
-                else:
-                    break
-            close_pipe(pipe1)
-            close_pipe(pipe2)
+            with Pipe(sys.argv[2], 'w') as p1, Pipe(sys.argv[3], 'w') as p2:
+                for line in sys.stdin:
+                    p1.writeline(line)
+                    p2.writeline(line)
 
         else:
             pass
+
     except:
         print(f"{sys.argv[0]}: error processing the command {command}.", file=sys.stderr)
         sys.exit(1)

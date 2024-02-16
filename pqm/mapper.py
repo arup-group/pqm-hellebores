6f55a414010fa478e05f933e2ce66a882b69f4a5
adam4521 <akjaworski@gmail.com>
2024-02-16 21:50:47 +0000

    removed bounds checking of vertical values, overscan is ok.

--- pqm/mapper.py
+++ pqm/mapper.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python3
 
-# Convert values into display coordinates, where the display is 800x600 pixels
-# (graphics to be 700x500 to leave space):
-# we'll make the x axis (time) from 0 to 699:
-# Where 0 pixel = 0ms and 699pixel = 40ms
-# x = int(t/40.0*699)
-# we'll make the vertical axis y (Voltage) from 0 to 479 pixels:
-# Where 0 pixel = 400V and 479pixel = -400V (negative because axis y goes down)
+# Convert values into display coordinates, where the display is 800x480 pixels
+# (graphics to be 700x480 to leave space for ui buttons)
+# therefore the x axis (time) from 0 to 699, the vertical axis y (voltage, current etc) from 0 to 479 pixels.
+# note that y=0 is top of screen, and y=479 is bottom of screen
 
 import sys
 import signal
 import settings
 
 
-def bound(lower, upper, value):
-    """Truncates a value to within a bounded minimum and maximum range"""
-    return max(lower, min(upper, value))
-
 def main():
    # load settings into st object from settings.json
    st = settings.Settings()
-   ymax = st.y_pixels-1
+   ymax = st.y_pixels
    for line in sys.stdin: # receive data from standard input
         try:
             ws = line.split()    # take this line and split on whitespace
@@ -34,27 +27,15 @@ def main():
             x = int(((float(t) + st.time_shift) 
                      * st.horizontal_pixels_per_division/st.time_axis_per_division)
                     % st.x_pixels)
-            # % st.y_pixels forces y coordinate to be between 0 and 479
-            y0 = bound(0, ymax, 
-                       (int(- float(c0)
-                            * st.vertical_pixels_per_division
-                            / st.voltage_axis_per_division) 
-                       + st.half_y_pixels))
-            y1 = bound(0, ymax,
-                       (int(- float(c1)
-                            * st.vertical_pixels_per_division
-                            /st.current_axis_per_division)
-                       + st.half_y_pixels))
-            y2 = bound(0, ymax,
-                       (int(- float(c2)
-                            * st.vertical_pixels_per_division
-                            / st.power_axis_per_division) 
-                       + st.half_y_pixels))
-            y3 = bound(0, ymax,
-                       (int(- float(c3)
-                            * st.vertical_pixels_per_division
-                            / st.earth_leakage_current_axis_per_division) 
-                       + st.half_y_pixels))
+            y0 = (int(- float(c0) * st.vertical_pixels_per_division / st.voltage_axis_per_division) 
+                      + st.half_y_pixels)
+            y1 = (int(- float(c1) * st.vertical_pixels_per_division /st.current_axis_per_division)
+                      + st.half_y_pixels)
+            y2 = (int(- float(c2) * st.vertical_pixels_per_division / st.power_axis_per_division) 
+                      + st.half_y_pixels)
+            y3 = (int(- float(c3) * st.vertical_pixels_per_division / st.earth_leakage_current_axis_per_division) 
+                      + st.half_y_pixels)
+           
             print(f'{x :4d} {y0 :4d} {y1 :4d} {y2 :4d} {y3 :4d} {em}')
             # make sure we get a clean flush of the buffer so that single shot (eg inrush) events
             # will be immediately displayed

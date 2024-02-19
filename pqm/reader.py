7ec8aed350c88c06e9f6e24f832bad0f2f23ae13
adam4521 <akjaworski@gmail.com>
2024-02-19 23:22:16 +0000

    added synchronisation to Pico and reader

--- pqm/reader.py
+++ pqm/reader.py
@@ -7,6 +7,53 @@ import sys
 import serial
 import settings
 
+BUFFER_SIZE = 128
+# 8 bytes per sample in buffer,
+# NB 4 bytes 0xff, 0xff, 0xff, 0xff are added to the end of each block
+# by the microcontroller for stream synchronisation and to resolve race condition
+# that can corrupt final byte
+BLOCK_SIZE = BUFFER_SIZE * 8
+
+
+def synchronise(ser):
+    # detect synchronisation bytes
+    # looking for 0xff, 0xff, don't care, don't care
+    try:
+        # try for a decent length of data
+        for _ in range (10000):
+            b = ser.read(1)
+            if b == 0xff:
+                b = ser.read(1)
+                if b == 0xff:
+                    b = ser.read(2)
+                    return True
+        raise ValueError
+    except:
+        return False
+
+
+def read_and_print(ser):
+    try:
+        # sometimes (rarely) there is a serial read error.
+        # this can be caused by the getty terminal process trying to read/write
+        # the serial port. We allow up to 5 successive re-tries.
+        retries = 5
+        while True:    
+            # read the entire block including the synchronisation bytes
+            bs = (ser.read(BLOCK_SIZE + 4).hex())
+            # process data as lines of 8 bytes, or 16 hex digits
+            for i in range(0, BLOCK_SIZE, 16):
+                print(f'{i//16 :04x} {bs[i:i+4]} {bs[i+4:i+8]} {bs[i+8:i+12]} {bs[i+12:i+16]}')
+            sys.stdout.flush()
+            retries = 5
+    except ValueError:
+        print('reader.py, main(): The data was not correct or complete.', file=sys.stderr)
+    except OSError:
+        print('reader.py, main(): Failed to read from serial port.', file=sys.stderr)
+        retries = retries - 1
+        if retries == 0:
+            print('reader.py, main(): Read error was persistent, quitting.', file=sys.stderr)
+
 
 
 def main():
@@ -20,27 +67,14 @@ def main():
         print("reader.py: Couldn't open serial port.", file=sys.stderr)
         sys.exit(1)
 
-    # sometimes (rarely) there is a serial read error.
-    # this can be caused by the getty terminal process trying to read/write
-    # the serial port. We allow up to 5 successive re-tries.
-    retries = 5
-    while True:    
-        try:
-            bs = (ser.read(256).hex())
-            # process data in chunks of 8 bytes, or 16 hex digits
-            for i in range(0, len(bs), 16):
-                print(f'{i//16 :04x} {bs[i:i+4]} {bs[i+4:i+8]} {bs[i+8:i+12]} {bs[i+12:i+16]}')
-            sys.stdout.flush()
-            retries = 5
-        except ValueError:
-            print('reader.py, main(): The data was not correct or complete.', file=sys.stderr)
-        except OSError:
-            print('reader.py, main(): Failed to read from serial port.', file=sys.stderr)
-            retries = retries - 1
-            if retries == 0:
-                print('reader.py, main(): Read error was persistent, quitting.', file=sys.stderr)
-                break
+    if not synchronise(ser):
+        print('reader.py: Failed to synchronise with incoming data.', file=sys.stderr)
+        ser.close()
+        sys.exit(1)
 
+    # enter the main loop, each read block corresponds with a block
+    # written by the microcontroller.
+    read_and_print(ser)
     ser.close()
 
 

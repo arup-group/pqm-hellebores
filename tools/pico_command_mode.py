3030e7836c8678e6ea268ef30689dc58e8d0cbb7
adam4521 <akjaworski@gmail.com>
2024-03-03 22:49:35 +0000

    temporary setup allows changes to settings via serial terminal

--- tools/pico_command_mode.py
+++ tools/pico_command_mode.py
@@ -9,16 +9,15 @@ gp.setmode(gp.BCM)
 gp.setup(MODE_PIN, gp.OUT)
 
 try:
-    # Set the mode select pin high for 60 seconds
+    # Set the mode select pin high for 300 seconds
     gp.output(MODE_PIN, True)
-    time.sleep(60)
+    time.sleep(300)
+
+except KeyError:
+    print('Interrupted.')
+
+finally:
+    # exit correctly
     gp.output(MODE_PIN, False)
     gp.cleanup()
-
-except:
-    # exit correctly
-    gp.cleanup()
-
-
-
 

bbcb9a4502ca8cc169229c916b7e875b92387780
adam4521 <akjaworski@gmail.com>
2024-03-26 22:05:07 +0000

    improved path switching and restoration

--- run/go.bat
+++ run/go.bat
@@ -1,7 +1,11 @@
 rem Simplified run script for Windows cmd.exe
 pushd .
 cd %~dp0
-if exist ..\venv-windows\ (call ..\venv-windows\scripts\activate)
+setlocal
+rem If there isn't one already active, activate the virtual environment
+if "%VIRTUAL_ENV%"=="" if exist ..\venv-windows\ call ..\venv-windows\scripts\activate
 python go.py
+endlocal
 popd
+
 

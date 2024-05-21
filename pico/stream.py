50d4df33923d2c895b87267eea65866e69e0dee0
adam4521 <akjaworski@gmail.com>
2024-05-21 23:05:00 +0100

    fixed syntax error in walrus expression, core 1 loop.

--- pico/stream.py
+++ pico/stream.py
@@ -337,7 +337,7 @@ def streaming_loop_core_1():
         # Inner loop -- speed critical -- we do sampling here, nothing else.
         while flags == STREAMING:
             # read out from the ADC *immediately* if the cell variable has changed
-            spi_adc_interface.readinto(cells_mv[cell_p := cell]) \
+            spi_adc_interface.readinto(cells_mv[(cell_p := cell)]) \
                 if cell != cell_p else None
         # If Core 0 has raised OVERLOAD flag, we deal with it here.
         if flags & OVERLOAD:

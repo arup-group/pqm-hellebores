6ae698be1e08d1d47fcf75813aec9f0e3ae3001f
Adam Jaworski <adam.jaworski@arup.com>
2024-07-27 13:41:57 +0100

    adding launcher scripts

--- pi-setup/wifi_setup.py
+++ pi-setup/wifi_setup.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3
 
+import sys
 import tkinter as tk
 
 root = tk.Tk()
+root.title('Wifi Setup')
 
-var1 = tk.IntVar()
-var2 = tk.StringVar()
+slider_position = tk.IntVar()
+character_selected = tk.StringVar()
+string_entered = tk.StringVar()
+
+def print_result(string_variable):
+    print(string_variable.get())
+
+# frame for the buttons
+button_frame = tk.Frame(root, width=400, height=100)
+button_frame.grid(row=2, column=0)
 
 # password field is an editable Text object
-text = tk.Text(root, height = 1, width = 50)
-text.pack()
+text = tk.Entry(root, width=50, textvariable=string_entered)
+text.grid(row=1, column=0)
 
 # we use a Button object to display the currently selected character
-character_button = tk.Button(root, width=3, font=('System', 72), textvariable=var2, \
-        command=lambda var=var2: text.insert(tk.END, var.get()))
-character_button.pack()
+type_button = tk.Button(button_frame, width=5, font=('System', 18), textvariable=character_selected, \
+        command=lambda c=character_selected: text.insert(tk.END, c.get()))
+type_button.grid(row=0, column=0)
+delete_button = tk.Button(button_frame, width=5, font=('System', 18), text='<BS', \
+        command=lambda: string_entered.set(string_entered.get()[:-1]))
+delete_button.grid(row=0, column=1)
+enter_button = tk.Button(button_frame, width=5, font=('System', 18), text='ENTER', \
+        command=lambda: print_result(string_entered) and sys.exit(0))
+enter_button.grid(row=0, column=2)
 
 # we use a Slider object to select a character
 selector = tk.Scale(root, from_=32, to=127, orient=tk.HORIZONTAL, length=500, \
-        width=50, label='Character selector', showvalue=0, variable=var1, \
-        command=lambda val, var=var1: var2.set(chr(var1.get())))
-selector.pack()
+        width=50, label='Character selector', showvalue=0, variable=slider_position, \
+        command=lambda val, var=slider_position: character_selected.set(chr(slider_position.get())))
+selector.grid(row=3, column=0)
+#selector.pack()
 
 # process events
 root.mainloop()

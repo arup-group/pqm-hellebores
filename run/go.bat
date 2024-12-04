32c767c0a039ffc23fe4dc78c3fc193ee0870256
Adam Jaworski <adam.jaworski@arup.com>
2024-12-04 16:12:05 +0000

    fixed path for .venv-windows

--- run/go.bat
+++ run/go.bat
@@ -1,9 +1,11 @@
 @rem Simplified run script for Windows cmd.exe
 @pushd .
+@rem Change to root directory of project
 @cd %~dp0
+@rem Make environment variables that we set have local scope only
 @setlocal
 @rem If there isn't one already active, activate the virtual environment
-@if "%VIRTUAL_ENV%"=="" if exist ..\venv-windows\ call ..\venv-windows\scripts\activate
+@if "%VIRTUAL_ENV%"=="" if exist ..\.venv-windows call ..\.venv-windows\scripts\activate
 @python go.py
 @endlocal
 @popd

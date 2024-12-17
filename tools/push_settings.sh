884726972461be293569e080437d549ebbf62f8b
Adam Jaworski <akjaworski@gmail.com>
2024-12-17 14:56:05 +0000

    fixed for mac

--- tools/push_settings.sh
+++ tools/push_settings.sh
@@ -5,7 +5,7 @@ import sys
 
 # send a signal to everyone to update their settings
 if os.name == 'posix':
-    os.system("pkill --signal=SIGUSR1 -f 'python3 \./.*\.py'")
+    os.system("pkill -SIGUSR1 -f 'python3 \./.*\.py'")
 else:
     print(f"This doesn't work on {os.name}", file=sys.stderr)
 

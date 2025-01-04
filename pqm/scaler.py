630a0d3fe3f32e17ae5d561a1163d9b7fa39f389
Adam Jaworski <akjaworski@gmail.com>
2025-01-04 17:32:25 +0000

    Improved error trapping of calibration constants

--- pqm/scaler.py
+++ pqm/scaler.py
@@ -23,21 +23,35 @@ def from_twos_complement_hex(w):
     v = int(w, base=16)
     return -(v & 0x8000) | (v & 0x7fff)
 
-def get_factors(run_calibrated=True):
-    global st, offsets, gains, delays, current_channel
+def set_current_channel():
+    global current_channel
     if st.current_sensor == 'low':
         current_channel = 1
     else:
         current_channel = 2
-    if run_calibrated == True:
+
+def set_uncalibrated_constants():
+    global offsets, gains, delays
+    offsets = [0, 0, 0, 0]
+    gains   = st.scale_factors
+    delays  = [-1, -1, -1, -1]
+
+def set_calibrated_constants():
+    global offsets, gains, delays
+    try:
         offsets = st.cal_offsets
         gains   = [ s*g for s,g in zip(st.scale_factors, st.cal_gains) ]
-        delays  = [ int(-1 - skew_time // st.interval) for skew_time in st.cal_skew_times ]
-    # we support running in 'uncalibrated' mode when we want to calibrate the device
-    else:
-        offsets = [0, 0, 0, 0]
-        gains   = st.scale_factors
-        delays  = [-1, -1, -1, -1]
+        delays  = []
+        for skew_time in st.cal_skew_times:
+            delay_shift = int(-1 - skew_time // st.interval)
+            # catch conditions where the requested delay is outside of the range of the delay line
+            if delay_shift < -(DELAY_LINE_LENGTH-1) or delay_shift > -1:
+                raise ValueError
+            delays.append(delay_shift)
+    except (NameError, ZeroDivisionError, TypeError, ValueError):
+        print('scaler.py, get_factors(): Error in setting calibration constants, '
+              'switching to uncalibrated.', file=sys.stderr)
+        set_uncalibrated_constants()
 
 def scale_readings(cs):
     """cs contains channel readings in integers"""
@@ -47,7 +61,7 @@ def scale_readings(cs):
 
 
 def main():
-    global st, current_channel
+    global st
 
     # check for uncalibrated mode
     if len(sys.argv) == 2 and sys.argv[1] == '--uncalibrated':
@@ -55,13 +69,18 @@ def main():
     else:
         run_calibrated = True
 
-    # NB get_factors() function will be called automatically when settings are changed
-    st = Settings(lambda run_calibrated=run_calibrated: get_factors(run_calibrated))
-    i = 0   # sample index
-    get_factors(run_calibrated)
+    # NB set_current_channel() function will be called automatically when
+    # settings are changed
+    st = Settings(lambda: set_current_channel())
 
     # the delay line allows timing skew between channels to be corrected
     delay_line = [ [0, 0, 0, 0] for i in range(DELAY_LINE_LENGTH) ]
+
+    i = 0   # sample index
+    set_current_channel()
+    set_calibrated_constants() if run_calibrated else set_uncalibrated_constants()
+    # delay_lookup contains the appropriate array offsets for delay in each channel
+    delay_lookup = list(zip([0,1,2,3], delays))
 
     # now loop over all the lines of data from stdin
     for line in sys.stdin:
@@ -70,12 +89,13 @@ def main():
             # calculate time axis position
             t = st.interval*i
             # split channel values into an integer array, removing the index field
-            cs = [ from_twos_complement_hex(w) for w in line.split()[1:] ]
-            # correct for channel timing skew and scale the readings
+            new_sample = [ from_twos_complement_hex(w) for w in line.split()[1:] ]
             delay_line = delay_line[1:]
-            delay_line.append(cs)
-            scaled = scale_readings([ delay_line[delays[ch]][ch] for ch in range(4) ])
-            # calculate floating point values, with appropriate calibration factors
+            delay_line.append(new_sample)
+            # use the delay line to correct for channel timing skew
+            # and apply calibration and scale factors to readings
+            scaled = scale_readings([ delay_line[delay][ch] for (ch, delay) in delay_lookup ])
+            # now pick out the individual readings ready for output
             voltage = scaled[3]
             # if st.current_axis_per_division is less than or equal to 0.1 A/div,
             # we use channel 1 for current measurements. If it is more than 0.1 A/div,

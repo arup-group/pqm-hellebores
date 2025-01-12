2250c43f417ed6a543aae8dc9bd684644dff9161
Adam Jaworski <akjaworski@gmail.com>
2025-01-12 22:26:24 +0000

    removed index from output

--- pqm/reader.py
+++ pqm/reader.py
@@ -45,7 +45,7 @@ def read_and_print(ser):
             bs = (ser.read(BLOCK_SIZE).hex())
             # process data as lines of 8 bytes, or 16 hex characters
             for i in range(0, BLOCK_SIZE*2, 16):
-                print(f'{i//16 :04x} {bs[i:i+4]} {bs[i+4:i+8]} {bs[i+8:i+12]} {bs[i+12:i+16]}')
+                print(f'{bs[i:i+4]} {bs[i+4:i+8]} {bs[i+8:i+12]} {bs[i+12:i+16]}')
             sys.stdout.flush()
             retries = 5
 

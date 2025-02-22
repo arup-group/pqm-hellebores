17abc4a9e9fe510bb7586e081389aeabea004843
Adam Jaworski <adam.jaworski@arup.com>
2025-02-22 11:04:23 +0000

    wip on performance verification

--- tools/line_speed.py
+++ tools/line_speed.py
@@ -4,16 +4,17 @@ import sys
 import time
 
 tl = int(time.time())
+t0 = tl
 i = 0
+ip = 0
 lps = 0
+lps_mean = 0
 for line in sys.stdin:
     t = int(time.time())
     if t != tl:
         tl = t
-        lps = i
-        i = 0
-    print(f'{lps} lps: {line.rstrip()}', end='\r')
-    i = i + 1
-
-
-
+        lps = i - ip
+        ip = i
+        lps_mean = int(i/(t - t0))
+    print(f'{lps} (average {lps_mean}) lps: {line.rstrip()}', end='\r')
+    i += 1

f4642872c46c7764591c15fee59722e340683ef6
Adam Jaworski <adam.jaworski@arup.com>
2025-02-26 11:20:49 +0000

    reverted to simple linespeed

--- tools/line_speed.py
+++ tools/line_speed.py
@@ -3,18 +3,15 @@
 import sys
 import time
 
-tl = int(time.time())
-t0 = tl
 i = 0
 ip = 0
 lps = 0
-lps_mean = 0
+tp = int(time.time())
 for line in sys.stdin:
-    t = int(time.time())
-    if t != tl:
-        tl = t
+    tn = int(time.time())
+    if tn != tp:
+        tp = tn
         lps = i - ip
         ip = i
-        lps_mean = int(i/(t - t0))
-    print(f'{lps} (average {lps_mean}) lps: {line.rstrip()}', end='\r')
+    print(f'{lps} lps: {line.rstrip()}', end='\r')
     i += 1
